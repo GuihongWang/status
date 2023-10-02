d28f1d137a90e6a0e195389763a1e95f367fd347
Guihong Wang <marisa@marisa.ml>
2023-10-02 17:21:05 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,15 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm795397634-e39449b3b7ab085a0ce55493', //rawgit
+	'm795397632-c5d9b551c73bc167d4a48f11', //git
+	'm795397628-195d4c2f4ff56b37663b6162', //file
+	'm795397823-6772b5f6161f7bd57388af48', //file-f
+	'm795397842-495e879e492a5a9abc41031b', //file-if
+	'm795397624-7f0ae008bcefbff98f3bf300', //blog
+	'm795397904-954f928f62648998a3c8489d', //blog-i
+	'm795397921-901571ed0f84a590d88a7f24', //git-f
+	'm795397933-28298ebbf40fa45e945ad07f', //rawgit-f
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
